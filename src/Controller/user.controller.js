ee4f867486c448631a365137854ef03f39ced50c
Gunjan Singh <gunjansingh72002@gmail.com>
2026-02-19 01:54:07 +0530

    updated

--- src/Controller/user.controller.js
+++ src/Controller/user.controller.js
@@ -1,4 +1,4 @@
-import { User } from "../models/user.model.js";
+import { User } from "../Models/user.model.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import getDataUri from "../Utils/datauri.js";
--- src/index.js
+++ src/index.js
@@ -1,23 +1,40 @@
-require("dotenv").config()
-const express = require("express")
-const app = express()
-const mongoose = require("mongoose")
-const cors = require("cors")
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
+import userRoute from "./routes/user.route.js";
+import companyRoute from "./routes/company.route.js";
+import jobRoute from "./routes/job.route.js";
+import applicationRoute from "./routes/application.route.js";
+
+dotenv.config({});
+
+const app = express();
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
+const corsOptions = {
+    origin:'http://localhost:5173',
+    credentials:true
+}
+
+app.use(cors(corsOptions));
+
+const PORT = process.env.PORT || 8080;
 
 
-app.use(cors({
-    origin: process.env.ORIGIN,
-    credentials: true
-}))
+// api's
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/company", companyRoute);
+app.use("/api/v1/job", jobRoute);
+app.use("/api/v1/application", applicationRoute);
 
-mongoose.connect(process.env.MONGO_URL)
-    .then(() => {
-        console.log("DB Connected")
-        app.listen(process.env.PORT, () => {
-            console.log(`Server connected on PORT ${process.env.PORT}`)
-        })
-    })
-    .catch((err) => {
-        console.log("DB Connection Failed")
-        console.log(err)
-    })
+
+
+app.listen(PORT,()=>{
+    connectDB();
+    console.log(`Server running at port ${PORT}`);
+})
